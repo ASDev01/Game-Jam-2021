535601ee9457cdb9478623baac2fad55b29e2299
ASDev01 <ASDev01@users.noreply.github.com>
2021-02-04 10:58:51 +0100

    Menu
    Menu básico

---
+++ Assets/Scripts/MyPlug-in/TextIn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextIn : MonoBehaviour
+{
+    public float inTime = 0f;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    void InAnim()
+    {
+
+    }
+}
